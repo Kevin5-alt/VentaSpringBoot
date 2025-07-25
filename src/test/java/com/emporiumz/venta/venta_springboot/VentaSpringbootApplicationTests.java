25a10c22126c9dff5c66114aba4e893cee935c84
Kevin <kevinceballos.gmail>
2025-07-25 12:04:41 -0500

    feat: agrega esqueleto de proyecto Spring Boot Java 21

---
+++ src/test/java/com/emporiumz/venta/venta_springboot/VentaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emporiumz.venta.venta_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
